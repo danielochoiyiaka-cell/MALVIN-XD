75342591a1530f6f7f9f32fb3a770a7b578fb4b3
danielochoiyiaka-cell <danielochoiyiaka@gmail.com>
2025-08-08 11:41:47 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~w7Ny1J7T#bv5nYUN-tdbqy5W0FFKpjlrtx7ZaPLvmKpdIOzO3I2Q",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2349022323035", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
